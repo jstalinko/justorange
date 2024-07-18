ea8ddc0c457c6c2a333f698319140e5ca26cd597
jstalinko <alinkodotid@gmail.com>
2024-07-18 20:00:25 +0700

    add: helpers.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-
+import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
@@ -17,4 +17,11 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+            '#utils' : path.resolve(__dirname,'resources/js/utils'),
+            '#helpers': path.resolve(__dirname,'resources/js/utils/helpers.js')
+        },
+    },
 });
